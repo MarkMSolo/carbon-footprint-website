d7ce0b705353c7e0ee67a777bbbaabddcdeea105
Mafutomin <jmarkmsolo123@gmail.com>
2024-07-25 22:24:00 -0700

    API Client for Carbon Footprint

--- src/App.js
+++ src/App.js
@@ -6,14 +6,27 @@ import "@cloudscape-design/global-styles/index.css";
 import {Input, Button} from "@cloudscape-design/components";
 
 function App() {
+
   const [value, setValue] = useState("google.com");
+
+  const calcCarbonFootprint = (searchTerm) => {
+    axios({
+      method: 'get',
+      url: `https://api.websitecarbon.com/site?url=${searchTerm}`,
+    })
+      .then(function (response) {
+        console.log(response);
+      });
+  }
+
   return (
     <div className="App">
       <Input
       onChange={({ detail }) => setValue(detail.value)}
       value={value}
     />
-    <Button variant="primary">Calculate website Carbon footprint</Button>
+    <Button onClick={calcCarbonFootprint(value)} 
+    variant="primary">Calculate website carbon footprint</Button>
     </div>
   );
 }
