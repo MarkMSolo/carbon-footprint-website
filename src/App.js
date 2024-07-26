465f852f88767d42f38f3dee1932851bca2a39b8
Mafutomin <jmarkmsolo123@gmail.com>
2024-07-25 22:41:16 -0700

    ES6 Syntax

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
       method: 'get',
       url: `https://api.websitecarbon.com/site?url=${searchTerm}`,
     })
-      .then(function (response) {
-        console.log(response);
+      .then((res) => {
+        console.log(res);
       });
   }
 
